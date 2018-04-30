c6a063d3dc74620b277c9dc674cfe56fbe18e00c
serenitydesire <rajdeepsaha_1980@yahoo.com>
2018-04-29 20:48:13 -0400

    updated lambda

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,5 @@
 import keystonemiddleware
 #Using ketstonemiddleware version 2.0 for vulnerability test
+#Do NOT run this function, testing features of a security software for code scan
 def lambda_handler(event, context):
     return "hello world"
